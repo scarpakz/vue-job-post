bbd2fe232713418c25c3240415d7edc012acb8f8
Jason Javier <jasonjjavier@gmail.com>
2026-01-10 13:38:01 -0400

    added homecardfinder for slot

--- src/components/CardFinder.vue
+++ src/components/CardFinder.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-            <div class="rounded-lg bg-indigo-200 px-4 py-4 lg:block md:block grid justify-center items-center">
+            <HomeCardFinder bg="bg-gray-100">
                 <h2 class="text-black font-bold text-3xl text-center md:text-left lg:text-3xl">For Developers</h2>
                 <p class="text-black/80 mb-5 text-center md:text-left">Explore jobs built for developers</p>
                 <button class="text-white bg-indigo-600 px-4 py-3 rounded-md cursor-pointer" type="button">Browse Jobs</button>
-            </div>
-            <div class="rounded-lg bg-gray-100 px-4 py-4 lg:block md:block grid justify-center items-center">
+            </HomeCardFinder>
+            <HomeCardFinder>
                 <h2 class="text-black font-bold text-3xl text-center md:text-left lg:text-3xl">For Employers</h2>
                 <p class="text-black/80 mb-5 text-center md:text-left">Post jobs and connect with developers that match your needs</p>
                 <button class="text-white bg-black px-4 py-3 rounded-md cursor-pointer" type="button">Add Job</button>
-            </div>
+            </HomeCardFinder>
         </div>
     </div>
 </template>
 <script setup>
-
+import HomeCardFinder from '@/components/HomeCardFinder.vue'
 </script>
---
+++ src/components/HomeCardFinder.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="`rounded-lg ${bg} px-4 py-4 lg:block md:block grid justify-center items-center`">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+defineProps({
+    bg: {
+        type: String,
+        default: 'bg-indigo-200'
+    }
+})
+</script>
