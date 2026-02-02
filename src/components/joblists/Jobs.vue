d96454897b71f21a0a9100107d8708f4f5623a0c
Jason Javier <jasonjjavier@gmail.com>
2026-02-01 21:43:14 -0400

    added getData() function and used axios library

--- src/components/joblists/Jobs.vue
+++ src/components/joblists/Jobs.vue
@@ -3,15 +3,20 @@
         <section class="py-16 bg-gray-50">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h2 class="text-3xl font-bold text-center mb-12">Available Jobs</h2>
-        
+                
                 <!-- Grid -->
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" v-if="!isLoading">
                     <Lists 
                     v-for="job in jobs.slice(0, limit || jobs.length)"
                     :job="job"
                     :key="job.id"
                     />
                 </div>
+                <Loader v-else>
+                    <span class="size-3 animate-ping rounded-full bg-indigo-600"></span>
+                    <span class="size-3 animate-ping rounded-full bg-indigo-600 [animation-delay:0.2s]"></span>
+                    <span class="size-3 animate-ping rounded-full bg-indigo-600 [animation-delay:0.4s]"></span>
+                </Loader>
             </div>
             <div class="flex justify-center mt-8"
             v-if="showButton"
@@ -29,10 +34,12 @@
 </template>
   
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 import { RouterLink } from 'vue-router'
 import Lists from '@/components/joblists/Lists.vue'
+import Loader from '@/components/Loader.vue'
 import joblists from '@/mockdata/joblists.json'
+import axios from 'axios'
 
 defineProps({
     limit: { 
@@ -43,6 +50,21 @@ defineProps({
         default: false
     }
 })
-const jobs = ref(joblists)
+const jobs = ref([])
+const isLoading = ref(true)
+
+/**
+ * Get all jobs
+ */
+const getData = async () => {
+    try {
+        const response = await axios.get('http://localhost:5000/jobs')
+        jobs.value = response.data
+    } catch (e) { console.error(e.status, e.message)
+    } finally { isLoading.value = false }
+}
+onMounted(() => {
+    getData()
+})
 
 </script>
