d3f9e24262683bf8423893b0867d078bd9920364
Jason Javier <jasonjjavier@gmail.com>
2026-02-21 16:55:58 -0400

    Update NavBar.vue
    Modify name key for routerlink on mobile link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -60,7 +60,7 @@
 
       <RouterLink
         @click="mobileOpen = false"
-        :to="{ name: 'home' }"
+        :to="{ name: 'add-job' }"
         class="block text-white hover:bg-white hover:text-indigo-800 rounded-md px-3 py-2 text-base font-medium"
       >Add Job</RouterLink>
     </div>
